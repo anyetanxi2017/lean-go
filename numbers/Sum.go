af0a14e08c5ddf1cbc5c8471ac90521d5b348d9a
anyetanxi2017 <50581436+anyetanxi2017@users.noreply.github.com>
2020-12-04 16:19:14 +0800

    切片 slice 基本使用

--- numbers/Sum.go
+++ numbers/Sum.go
@@ -1,8 +1,10 @@
 package numbers
 
-func Sum(numbers [5]int) (sum int) {
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+func Sum(numbers []int) (sum int) {
+	//range 会迭代数组，每次迭代都会返回数组元素的索引和值。
+	// 我们选择使用_空白标志符来忽略索引。
+	for _, numbers := range numbers {
+		sum += numbers
 	}
 	return
 }
--- numbers/sum_test.go
+++ numbers/sum_test.go
@@ -3,10 +3,14 @@ package numbers
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if want != got {
-		t.Errorf("got %d want %d given,%v", got, want, numbers)
-	}
+
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		//numbers := [5]int{1, 2, 3, 4, 5} 声明时不指定长度就是切片 指定则是数组
+		got := Sum(numbers)
+		want := 15
+		if want != got {
+			t.Errorf("got %d want %d given,%v", got, want, numbers)
+		}
+	})
 }
