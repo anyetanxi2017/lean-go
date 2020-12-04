d25fa8101000fedb44228e14d1dab6491a19d836
anyetanxi2017 <50581436+anyetanxi2017@users.noreply.github.com>
2020-12-05 01:47:48 +0800

    结构体、方法和接口

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,6 +1,12 @@
 package structs
 
-import "math"
+import (
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+}
 
 // 长方形
 type Rectangle struct {
@@ -14,15 +20,22 @@ type Circle struct {
 	Radius float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+// 三角形
+type Triangle struct {
+	Base   float64
+	Height float64
 }
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
-}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+func (c Triangle) Area() float64 {
+	return (c.Base * c.Height) * 0.5
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -16,23 +16,46 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	//长方式面积
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
+	//checkArea := func(t *testing.T, shape Shape, want float64) {
+	//	t.Helper()
+	//	got := shape.Area()
+	//	if got != want {
+	//		t.Errorf("got %.2f want %.2f", got, want)
+	//	}
+	//}
+	//
+	////长方式面积
+	//t.Run("rectangles", func(t *testing.T) {
+	//	rectangle := Rectangle{12.0, 6.0}
+	//	checkArea(t, rectangle, 72.0)
+	//
+	//})
+	//
+	//// 圆形面积
+	//t.Run("circles", func(t *testing.T) {
+	//	circle := Circle{10}
+	//	checkArea(t, circle, 314.1592653589793)
+	//
+	//})
 
-	// 圆形面积
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+	// 表格驱动测试方式
+	// 列表驱动测试可以成为你工具箱中的得力武器。
+	// 如果你要测试一个接口的不同实现，或者传入函数的数据有很多不同的测试需求，这个武器非常给力。
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		//{Rectangle{12,6},72.0},
+		//{Circle{10},314.1592653589793},
+		// 为了让测试函数更容易理解 可以选择命名下面的域
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
 		}
-	})
+	}
+	// 我们可以通过如下命令来运行列表中指定的测试用例： go test -run TestArea/Rectangle
 }
