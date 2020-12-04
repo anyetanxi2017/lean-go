abf62d39dcdb77e408683d1eaaf0be1877b4307a
anyetanxi2017 <50581436+anyetanxi2017@users.noreply.github.com>
2020-12-04 16:05:12 +0800

    数组的使用

---
+++ iteration/repeat_test.go
@@ -0,0 +1,29 @@
+package iteration
+
+import (
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+	if repeated != expected {
+		t.Errorf("expected '%q' bug got '%q'", expected, repeated)
+	}
+}
+
+const repeatCount = 5
+
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
