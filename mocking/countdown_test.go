9a2afbbce011019576b68055c3290000fcfa5e91
anyetanxi2017 <50581436+anyetanxi2017@users.noreply.github.com>
2020-12-05 15:29:04 +0800

    mocking 完成

---
+++ mocking/countdown_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+	Countdown(buffer,spySleeper)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
