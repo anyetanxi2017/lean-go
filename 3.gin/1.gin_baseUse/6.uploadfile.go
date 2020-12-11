6a343c928d268e563d1f55def285667cc6f6019c
anyetanxi2017 <50581436+anyetanxi2017@users.noreply.github.com>
2020-12-12 03:13:19 +0800

    多文件上传

--- 3.gin/1.gin_baseUse/6.uploadfile.go
+++ 3.gin/1.gin_baseUse/6.uploadfile.go
@@ -22,5 +22,25 @@ func main() {
 			c.JSON(http.StatusOK, gin.H{"msg": "ok"})
 		}
 	})
+
+	// 多个文件上传
+	// 处理multipart forms提交文件时默认的内存限制是32 MiB
+	// 可以通过下面的方式修改
+	// router.MaxMultipartMemory = 8 << 20  // 8 MiB
+	r.POST("/uploadMultipartForm", func(c *gin.Context) {
+		// Multipart form
+		form, _ := c.MultipartForm()
+		files := form.File["file"]
+
+		for index, file := range files {
+			log.Println(file.Filename)
+			dst := fmt.Sprintf("C:/tmp/%s_%d", file.Filename, index)
+			// 上传文件到指定的目录
+			c.SaveUploadedFile(file, dst)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("%d files uploaded!", len(files)),
+		})
+	})
 	r.Run()
 }
