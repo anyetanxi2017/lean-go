10c351b37f75bce15da14c3b9e5e1fd9fa1ad9ba
anyetanxi2017 <rita413@outlook.com>
2020-12-15 03:12:06 +0800

    跨域中间件定义及使用

---
+++ 3.gin/middlewares/mycors.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(MyCors)
+	r.Run()
+}
+
+//设置跨域让访问的逻辑
+func MyCors(c *gin.Context) {
+
+	method := c.Request.Method
+	origin := c.GetHeader("Origin")
+
+	//如果是本地访问,则URL地址为 http://localhost:8081
+	if origin == "http://localhost:8081" {
+		var headerKeys []string
+		for k, _ := range c.Request.Header {
+			headerKeys = append(headerKeys, k)
+		}
+		headerStr := strings.Join(headerKeys, ", ")
+		if headerStr != "" {
+			headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
+		} else {
+			headerStr = "access-control-allow-origin, access-control-allow-headers"
+		}
+		//c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", headerStr)
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
+		//c.Header("Access-Control-Allow-Headers", "*")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Set("content-type", "application/json")
+	}
+
+	if method == "OPTIONS" {
+		c.JSON(http.StatusOK, "Options Request!")
+	}
+
+	c.Next()
+
+}
