aafa9c9c5346a19052f1ec3ac321d49e5cc2b0d3
anyetanxi2017 <50581436+anyetanxi2017@users.noreply.github.com>
2020-12-26 10:49:04 +0800

    设置下次执行时间

--- 6.cron/main.go
+++ 6.cron/main.go
@@ -1,13 +1,11 @@
-package main
+package cron
 
 import (
 	"fmt"
 	"github.com/robfig/cron/v3"
-	"time"
 )
 
 var entryId cron.EntryID = 0
-var num = 0
 var c = cron.New()
 
 type MyJob struct {
@@ -19,18 +17,17 @@ func (j MyJob) GetInterval() string {
 }
 
 func (j MyJob) Run() {
-	num++
-	fmt.Println(time.Now())
-	if num%2 == 0 {
-		c.Remove(entryId)
-		fmt.Println("快 修改时间")
-		myJob := MyJob{5}
-		entryId, _ = c.AddJob(myJob.GetInterval(), myJob)
-	}
+	j.SetNextTime(5)
+}
+
+// 设置下次执行时间
+func (j MyJob) SetNextTime(interval int) {
+	c.Remove(entryId)
+	myJob := MyJob{interval}
+	entryId, _ = c.AddJob(myJob.GetInterval(), myJob)
 }
 
 func Start() {
-	myJob := MyJob{10}
-	entryId, _ = c.AddJob(myJob.GetInterval(), myJob)
+	MyJob{}.SetNextTime(10)
 	c.Start()
 }
