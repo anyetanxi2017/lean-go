f3b0f0c43f8bdc595872b4029f505745a1e14145
anyetanxi2017 <50581436+anyetanxi2017@users.noreply.github.com>
2021-03-04 21:28:40 +0700

    Create numUtil.go

---
+++ utils/numUtil.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+type NumUtil struct {
+}
+
+func (n NumUtil) RandomFloat(min, max float64) float64 {
+	if max == min {
+		return max
+	}
+	return n.decimal(rand.Float64()*(max-min) + min)
+}
+func (n NumUtil) RandomFloatStr(minStr, maxStr string) float64 {
+	min, err := strconv.ParseFloat(minStr, 64)
+	if err != nil {
+		panic(err)
+	}
+	max, err := strconv.ParseFloat(maxStr, 64)
+	if err != nil {
+		panic(err)
+	}
+	return n.RandomFloat(min, max)
+}
+func (n NumUtil) decimal(v float64) float64 {
+	val, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", v), 64)
+	return val
+}
+
+func (n NumUtil) RandomInt(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Intn(max-min) + min
+	return randNum
+}
+
+func (n NumUtil) StrToFloat(s string) float64 {
+	if float, err := strconv.ParseFloat(s, 10); err != nil {
+		panic(err)
+	} else {
+		return float
+	}
+	return 0
+}
